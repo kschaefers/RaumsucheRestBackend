a1d46966a055f2df3d0a415a134e7f94256148cc
kschaefers <online@kschaefers.de>
2015-12-10 15:30:11 +0100

    changed password of database

--- db.php
+++ db.php
@@ -14,7 +14,7 @@ class db
     static function getPDO() {
         $dsn = 'mysql:dbname=d02043d3;host=127.0.0.1';
         $user = 'd02043d3';
-        $password = 'h8rQLgrhrRNZoZGw';
+        $password = 'vD6TAg5fXgduY2fM';
         static $db = null;
         if (null === $db)
             $db = new PDO($dsn,$user,$password);
