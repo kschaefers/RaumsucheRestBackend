710ce575653ba61fe257f8cb35d80d340ebe4c2e
sereri <christoph.r@gmx.net>
2015-12-17 15:53:34 +0100

    removed [Termin] from results

--- parser.php
+++ parser.php
@@ -30,10 +30,11 @@ class parser
 
                 $aArray = $td->find('a');
                 foreach($aArray as $a){
-                    $roomArray = explode('&nbsp;',$a->plaintext);
-                    $options = substr($roomArray[2], 0, strlen($roomArray[2]) - 1);
-                    $st = $pdo->prepare(
-                    "INSERT INTO rooms SET
+                    if(strpos($a->plaintext,"[") === false) {
+                        $roomArray = explode('&nbsp;', $a->plaintext);
+                        $options = substr($roomArray[2], 0, strlen($roomArray[2]) - 1);
+                        $st = $pdo->prepare(
+                            "INSERT INTO rooms SET
                         Name = :name,
                         Size = :size,
                         Day = :day,
@@ -43,18 +44,19 @@ class parser
                         Pool = :pool,
                         LooseSeating = :looseSeating,
                         Video = :video"
-                    );
-                    $st->execute(array(
-                        ':name' => $roomArray[0],
-                        ':size' => substr($roomArray[1], 1, strlen($roomArray[1]) - 2),
-                        ':day' => $tdKey,
-                        ':hour' => $trKey,
-                        ':computer' => ((strpos($options,'C') !== false)?1:0),
-                        ':beamer' => ((strpos($options,'B') !== false)?1:0),
-                        ':pool' => ((strpos($options,'P') !== false)?1:0),
-                        ':looseSeating' => ((strpos($options,'L') !== false)?1:0),
-                        ':video' => ((strpos($options,'V') !== false)?1:0),
-                    ));
+                        );
+                        $st->execute(array(
+                            ':name' => $roomArray[0],
+                            ':size' => substr($roomArray[1], 1, strlen($roomArray[1]) - 2),
+                            ':day' => $tdKey,
+                            ':hour' => $trKey,
+                            ':computer' => ((strpos($options, 'C') !== false) ? 1 : 0),
+                            ':beamer' => ((strpos($options, 'B') !== false) ? 1 : 0),
+                            ':pool' => ((strpos($options, 'P') !== false) ? 1 : 0),
+                            ':looseSeating' => ((strpos($options, 'L') !== false) ? 1 : 0),
+                            ':video' => ((strpos($options, 'V') !== false) ? 1 : 0),
+                        ));
+                    }
                 }
             }
         }
